b14b8ee2246dc7a7b01563d2a3ed07b1a61b6463
H0nneur <honneur0@hotmail.com>
2023-03-27 20:16:50 +0200

    adding of quicksand fontfamily

--- app/src/main/java/com/gdsc/recyclr/ui/theme/Type.kt
+++ app/src/main/java/com/gdsc/recyclr/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.gdsc.recyclr.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.gdsc.recyclr.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -25,4 +27,12 @@ val Typography = Typography(
         fontSize = 12.sp
     )
     */
+)
+
+val quicksandFamily = FontFamily(
+    Font(R.font.quicksand_bold, FontWeight.Bold),
+    Font(R.font.quicksand_light, FontWeight.Light),
+    Font(R.font.quicksand_medium, FontWeight.Medium),
+    Font(R.font.quicksand_regular, FontWeight.Normal),
+    Font(R.font.quicksand_semibold, FontWeight.SemiBold)
 )
